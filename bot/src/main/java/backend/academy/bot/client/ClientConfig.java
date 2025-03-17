b21c754fa56354ce1239c8f5c49e4a93005c4edd
Ivan Kramarenko <i.m.kramarenko@tbank.ru>
2025-03-17 11:10:00 +0300

    [hw-1] feat: Log and handle api error

--- bot/src/main/java/backend/academy/bot/client/ClientConfig.java
+++ bot/src/main/java/backend/academy/bot/client/ClientConfig.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Configuration
@@ -21,20 +22,46 @@ public class ClientConfig {
             .baseUrl("http://localhost:8081") // TODO Set independent url
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-            .filter((request, next) -> {
-                log.info("Request: {} {}", request.method(), request.url());
-                return next.exchange(request)
-                    .doOnNext(response -> log.info("Response: {}", response.statusCode()))
-                    .doOnError(WebClientResponseException.class, e -> {
-                        ApiErrorResponse error = e.getResponseBodyAs(ApiErrorResponse.class);
-                        if (error == null) {
-                            log.error("API Error code: {}, can't retrieve original error", e.getStatusCode());
-                        } else {
-                            log.error("API Error: {} - {}", e.getStatusCode(), error.getExceptionMessage());
-                        }
-                    });
-            })
-            .build(); // TODO Add retries
+            .filter(logRequest())
+            .filter(logResponse())
+            .filter(errorHandler())
+            .build();
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(request -> {
+            log.info("Request: {} {}", request.method(), request.url());
+            request.headers().forEach((name, values) ->
+                values.forEach(value -> log.debug("Request header: {}={}", name, value)));
+            return Mono.just(request);
+        });
+    }
+
+    private ExchangeFilterFunction logResponse() {
+        return ExchangeFilterFunction.ofResponseProcessor(response -> {
+            log.info("Response status: {}", response.statusCode());
+            response.headers().asHttpHeaders().forEach((name, values) ->
+                values.forEach(value -> log.debug("Response header: {}={}", name, value)));
+            return Mono.just(response);
+        });
+    }
+
+    private ExchangeFilterFunction errorHandler() {
+        return ExchangeFilterFunction.ofResponseProcessor(response -> {
+            if (response.statusCode().isError()) {
+                return response.bodyToMono(ApiErrorResponse.class)
+                    .flatMap(errorBody -> {
+                        log.error("API Error: {} - {} | Exception name: {} | Exception message: {}",
+                            errorBody.getCode(),
+                            errorBody.getDescription(),
+                            errorBody.getExceptionName(),
+                            errorBody.getExceptionMessage());
+                        return Mono.error(new ApiException(errorBody));
+                    })
+                    .thenReturn(response);
+            }
+            return Mono.just(response);
+        });
     }
 
     @Bean
