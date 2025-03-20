5ec0e584dc2dd55563a67c67314159f3c3cd846e
Ivan Kramarenko <i.m.kramarenko@tbank.ru>
2025-03-20 07:28:34 +0300

    [hw-1] feat: Add Router test

---
+++ bot/src/test/java/backend/academy/bot/state/RouterTest.java
@@ -0,0 +1,82 @@
+package backend.academy.bot.state;
+
+import backend.academy.bot.state.handler.Handler;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Message;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RouterTest {
+
+    @Mock
+    private Handler handler1;
+
+    @Mock
+    private Handler handler2;
+
+    @Spy
+    private ArrayList<Handler> handlers;
+
+    @InjectMocks
+    private Router router;
+
+    @BeforeEach
+    public void setUp() {
+        handlers.add(handler1);
+        handlers.add(handler2);
+    }
+
+    @Test
+    @DisplayName("First handler handle")
+    void firstHandle() {
+        when(handler1.handle(any())).thenReturn(true);
+        when(handler1.nextState()).thenReturn(State.TRACK_FILTERS);
+
+        verify(handler2, never()).handle(any());
+        verify(handler2, never()).nextState();
+
+        assertEquals(State.TRACK_FILTERS, router.process(generateHandlerContext()));
+    }
+
+    @Test
+    @DisplayName("Second handler handle")
+    void secondHandle() {
+        when(handler1.handle(any())).thenReturn(false);
+        verify(handler1, never()).nextState();
+
+        when(handler2.handle(any())).thenReturn(true);
+        when(handler2.nextState()).thenReturn(State.TRACK_LINK);
+
+        assertEquals(State.TRACK_LINK, router.process(generateHandlerContext()));
+    }
+
+    @Test
+    @DisplayName("No handler handle")
+    void noHandle() {
+        when(handler1.handle(any())).thenReturn(false);
+        verify(handler1, never()).nextState();
+
+        when(handler2.handle(any())).thenReturn(false);
+        verify(handler2, never()).nextState();
+
+
+        assertEquals(State.MENU, router.process(generateHandlerContext()));
+    }
+
+    private static HandlerContext generateHandlerContext() {
+        return new HandlerContext(new Message(), new TelegramBot("123"), State.START);
+    }
+}
