cb7cf2150e8bde5fdc61b450c0a0f4ded87a27cc
Ivan Kramarenko <i.m.kramarenko@tbank.ru>
2025-03-24 15:50:34 +0300

    [hw-1] fix: Fix formatting

--- bot/src/main/java/backend/academy/bot/util/MessageUtil.java
+++ bot/src/main/java/backend/academy/bot/util/MessageUtil.java
@@ -6,10 +6,16 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MessageUtil {
     public static String linkMessage(LinkResponse linkResponse) {
-        return String.format(
-                "\uD83C\uDF10 %s%n• \uD83D\uDD16 %s%n• \uD83D\uDD0D %s",
-                linkResponse.getUrl(),
-                String.join(" ", linkResponse.getTags()),
-                String.join(" ", linkResponse.getFilters()));
+        StringBuilder sb = new StringBuilder();
+        sb.append("\uD83C\uDF10 %s".formatted(linkResponse.getUrl()));
+        if (!linkResponse.getTags().isEmpty()) {
+            sb.append('\n');
+            sb.append("• \uD83D\uDD16 %s".formatted(linkResponse.getTags()));
+        }
+        if (!linkResponse.getFilters().isEmpty()) {
+            sb.append('\n');
+            sb.append("• \uD83D\uDD0D %s".formatted(linkResponse.getFilters()));
+        }
+        return sb.toString();
     }
 }
