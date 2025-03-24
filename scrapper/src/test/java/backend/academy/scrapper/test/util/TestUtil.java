deff14d41fb608d8af1ffb49ef7221df98226826
Ivan Kramarenko <i.m.kramarenko@tbank.ru>
2025-03-24 20:17:17 +0300

    [hw-1] test: Add LinkService test

---
+++ scrapper/src/test/java/backend/academy/scrapper/test/util/TestUtil.java
@@ -0,0 +1,36 @@
+package backend.academy.scrapper.test.util;
+
+import backend.academy.scrapper.parser.LinkType;
+import backend.academy.scrapper.repository.LinkRecord;
+import io.micrometer.tracing.Link;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+@UtilityClass
+public class TestUtil {
+
+    @SneakyThrows
+    public static LinkRecord generateLinkRecord(long id, String url, List<String> tags, List<String> filters, LinkType linkType) {
+        return new LinkRecord(id, new URI(url), tags, filters, linkType);
+    }
+
+    public static LinkRecord generateLinkRecord(long id, String url) {
+        return generateLinkRecord(id, url, null);
+    }
+
+    public static LinkRecord generateLinkRecord(long id, String url, LinkType linkType) {
+        return generateLinkRecord(id, url, Collections.emptyList(), Collections.emptyList(), linkType);
+    }
+
+    public static LinkRecord generateLinkRecord(long id, String url, List<String> tags, List<String> filters) {
+        return generateLinkRecord(id, url, tags, filters, null);
+    }
+
+    public static long generateLong() {
+        return new Random().nextLong(1L, 1000L);
+    }
+}
