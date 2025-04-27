efdc52db88eb5c4e5cc88160aa900ec782a88cbc
Ivan Kramarenko <ivankramarenko13@gmail.com>
2025-04-27 17:44:12 +0300

    [hw-2] test: Fix test environment

--- bot/src/test/java/backend/academy/bot/service/UpdateServiceTest.java
+++ bot/src/test/java/backend/academy/bot/service/UpdateServiceTest.java
@@ -1,5 +1,24 @@
 package backend.academy.bot.service;
 
+import backend.academy.bot.state.HandlerContext;
+import backend.academy.bot.state.Router;
+import backend.academy.bot.state.State;
+import backend.academy.bot.test.utils.TestUtil;
+import ch.qos.logback.classic.Level;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import java.util.List;
+import java.util.ResourceBundle;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static backend.academy.bot.test.utils.LoggerTestUtil.appenderContainsLog;
 import static backend.academy.bot.test.utils.LoggerTestUtil.getListAppender;
 import static backend.academy.bot.test.utils.TestUtil.generateLinkUpdate;
@@ -13,25 +32,6 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import backend.academy.bot.state.HandlerContext;
-import backend.academy.bot.state.Router;
-import backend.academy.bot.state.State;
-import backend.academy.bot.test.utils.TestUtil;
-import ch.qos.logback.classic.Level;
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.SendMessage;
-import java.util.List;
-import java.util.ResourceBundle;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateServiceTest {
@@ -51,83 +51,90 @@ class UpdateServiceTest {
     @InjectMocks
     private UpdateService updateService;
 
-    @Test
-    @DisplayName("Do nothing on null")
-    void doNothingOnNull() {
-        Update update = generateUpdate(null);
+    @Nested
+    class UpdatesFromTelegram {
+        @Test
+        @DisplayName("Do nothing on null")
+        void doNothingOnNull() {
+            Update update = generateUpdate(null);
 
-        updateService.updateProcess(update);
+            updateService.updateProcess(update);
 
-        verify(stateService, never()).getState(any());
-        verify(stateService, never()).setState(any(), any());
-        verify(router, never()).process(any());
-        verify(telegramBot, never()).execute(any());
-    }
+            verify(stateService, never()).getState(any());
+            verify(stateService, never()).setState(any(), any());
+            verify(router, never()).process(any());
+            verify(telegramBot, never()).execute(any());
+        }
 
-    @Test
-    @DisplayName("Happy path")
-    void happyPath() {
-        var appender = getListAppender(UpdateService.class);
+        @Test
+        @DisplayName("Happy path")
+        void happyPath() {
+            var appender = getListAppender(UpdateService.class);
 
-        Message message = generateMessage("text", 123L);
-        Update update = generateUpdate(message);
+            Message message = generateMessage("text", 123L);
+            Update update = generateUpdate(message);
 
-        State state = State.TRACK_FILTERS;
-        State nextState = State.MENU;
-        when(stateService.getState(anyLong())).thenReturn(state);
-        when(router.process(any())).thenReturn(nextState);
+            State state = State.TRACK_FILTERS;
+            State nextState = State.MENU;
+            when(stateService.getState(anyLong())).thenReturn(state);
+            when(router.process(any())).thenReturn(nextState);
 
-        updateService.updateProcess(update);
+            updateService.updateProcess(update);
 
-        verify(router).process(new HandlerContext(message, telegramBot, state));
-        verify(stateService).setState(123L, nextState);
-        verify(telegramBot, never()).execute(any());
+            verify(router).process(new HandlerContext(message, telegramBot, state));
+            verify(stateService).setState(123L, nextState);
+            verify(telegramBot, never()).execute(any());
 
-        assertTrue(appenderContainsLog(
+            assertTrue(appenderContainsLog(
                 appender,
                 Level.INFO,
                 "Message chatId: %s, current state: %s, text: %s".formatted(123L, state, message.text())));
+        }
+
+        @Test
+        @DisplayName("Router throw Exception")
+        void exception() {
+            var appender = getListAppender(UpdateService.class);
+
+            Message message = generateMessage("text", 123L);
+            Update update = generateUpdate(message);
+
+            State state = State.TRACK_FILTERS;
+            when(stateService.getState(anyLong())).thenReturn(state);
+            when(router.process(any())).thenThrow(new RuntimeException("exception"));
+            when(resourceBundle.getString("error.message")).thenReturn("Error");
+
+            updateService.updateProcess(update);
+
+            ArgumentCaptor<SendMessage> argumentCaptor = ArgumentCaptor.forClass(SendMessage.class);
+            verify(stateService, times(1)).setState(123L, State.MENU);
+            verify(telegramBot, times(1)).execute(argumentCaptor.capture());
+            SendMessage sendMessage = argumentCaptor.getValue();
+            assertEquals("Error", TestUtil.getText(sendMessage));
+
+            assertTrue(appenderContainsLog(appender, Level.ERROR, "Exception while routing occurred"));
+        }
     }
 
-    @Test
-    @DisplayName("Router throw Exception")
-    void exception() {
-        var appender = getListAppender(UpdateService.class);
-
-        Message message = generateMessage("text", 123L);
-        Update update = generateUpdate(message);
-
-        State state = State.TRACK_FILTERS;
-        when(stateService.getState(anyLong())).thenReturn(state);
-        when(router.process(any())).thenThrow(new RuntimeException("exception"));
-        when(resourceBundle.getString("error.message")).thenReturn("Error");
-
-        updateService.updateProcess(update);
-
-        ArgumentCaptor<SendMessage> argumentCaptor = ArgumentCaptor.forClass(SendMessage.class);
-        verify(stateService, times(1)).setState(123L, State.MENU);
-        verify(telegramBot, times(1)).execute(argumentCaptor.capture());
-        SendMessage sendMessage = argumentCaptor.getValue();
-        assertEquals("Error", TestUtil.getText(sendMessage));
-
-        assertTrue(appenderContainsLog(appender, Level.ERROR, "Exception while routing occurred"));
-    }
-
-    @Test
+    @Nested
     @DisplayName("Update send update for received linkUpdate message")
-    void sendUpdatesToChats() {
-        var linkUpdate = generateLinkUpdate(1L, 2L, 3L, 4L);
-        when(resourceBundle.getString("update.format.message")).thenReturn("%s and %s");
-        updateService.updateProcess(linkUpdate);
+    class UpdatesFromScrapper {
+        @Test
+        @DisplayName("Updates for links 1, 2, 3, 4")
+        void happyPath() {
+            var linkUpdate = generateLinkUpdate(1L, 2L, 3L, 4L);
+            when(resourceBundle.getString("update.format.message")).thenReturn("%s and %s");
+            updateService.updateProcess(linkUpdate);
 
-        ArgumentCaptor<SendMessage> argumentCaptor = ArgumentCaptor.forClass(SendMessage.class);
-        verify(telegramBot, times(4)).execute(argumentCaptor.capture(), any());
+            ArgumentCaptor<SendMessage> argumentCaptor = ArgumentCaptor.forClass(SendMessage.class);
+            verify(telegramBot, times(4)).execute(argumentCaptor.capture(), any());
 
-        var values = argumentCaptor.getAllValues().iterator();
-        for (var i : List.of(1L, 2L, 3L, 4L)) {
-            SendMessage sendMessage = values.next();
-            assertEquals("description and url", TestUtil.getText(sendMessage));
-            assertEquals(i, TestUtil.getId(sendMessage));
+            var values = argumentCaptor.getAllValues().iterator();
+            for (var i : List.of(1L, 2L, 3L, 4L)) {
+                SendMessage sendMessage = values.next();
+                assertEquals("description and url", TestUtil.getText(sendMessage));
+                assertEquals(i, TestUtil.getId(sendMessage));
+            }
         }
     }
 }
