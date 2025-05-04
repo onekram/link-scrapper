4250662c26bfae4fa53c763efed54addc71a8d0e
Ivan Kramarenko <ivankramarenko13@gmail.com>
2025-05-05 01:31:39 +0300

    [hw-2] feat: Delete link if no subscription

--- scrapper/src/main/java/backend/academy/scrapper/repository/ChatRepository.java
+++ scrapper/src/main/java/backend/academy/scrapper/repository/ChatRepository.java
@@ -6,7 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    void deleteChatById(Long id);
-
-    boolean existsChatById(Long id);
 }
--- scrapper/src/main/java/backend/academy/scrapper/service/ChatService.java
+++ scrapper/src/main/java/backend/academy/scrapper/service/ChatService.java
@@ -2,7 +2,10 @@ package backend.academy.scrapper.service;
 
 import backend.academy.scrapper.exception.NotFoundException;
 import backend.academy.scrapper.repository.ChatRepository;
+import backend.academy.scrapper.repository.LinkRepository;
 import backend.academy.scrapper.repository.entity.Chat;
+import backend.academy.scrapper.repository.entity.Link;
+import backend.academy.scrapper.repository.entity.Subscription;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChatService {
     private final ChatRepository chatRepository;
+    private final LinkRepository linkRepository;
 
     public void register(Long id) {
         chatRepository.save(new Chat(id));
@@ -18,10 +22,16 @@ public class ChatService {
 
     @Transactional
     public void unRegister(Long id) {
-        if (chatRepository.existsChatById(id)) {
-            chatRepository.deleteChatById(id);
-        } else {
-            throw new NotFoundException(String.format("Не существует чата с ID: %s", id));
+        Chat chat = chatRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Не существует чата с ID: %s", id)));
+        chat.subscriptions().forEach(this::deleteSubscription);
+        chatRepository.delete(chat);
+    }
+
+    private void deleteSubscription(Subscription subscription) {
+        Link link = subscription.link();
+        link.subscriptions().remove(subscription);
+        if (link.subscriptions().isEmpty()) {
+            linkRepository.delete(link);
         }
     }
 }
