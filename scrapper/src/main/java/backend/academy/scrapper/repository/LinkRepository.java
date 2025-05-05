0bedb87b79c9a723e7a009497f321925ca34aef5
Ivan Kramarenko <ivankramarenko13@gmail.com>
2025-05-05 18:49:49 +0300

    [hw-2] feat: Add EntityGraph to findAllByType method for non-lazy fetching

--- scrapper/src/main/java/backend/academy/scrapper/repository/LinkRepository.java
+++ scrapper/src/main/java/backend/academy/scrapper/repository/LinkRepository.java
@@ -5,6 +5,7 @@ import backend.academy.scrapper.repository.entity.Link;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,6 @@ import org.springframework.stereotype.Repository;
 public interface LinkRepository extends JpaRepository<Link, Long> {
     Optional<Link> findByUrl(String url);
 
+    @EntityGraph(attributePaths = {"subscriptions", "subscriptions.chat"})
     List<Link> findAllByType(LinkType type, Pageable pageable);
 }
