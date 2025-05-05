be7b491854f9b529c76fdfa00957e3cb7c07e4c4
Ivan Kramarenko <ivankramarenko13@gmail.com>
2025-05-05 19:05:41 +0300

    [hw-2] feat: Add ORM and SQL test configurations for ScrapperApplicationTests

---
+++ scrapper/src/test/java/backend/academy/scrapper/OrmScrapperApplicationTests.java
@@ -0,0 +1,18 @@
+package backend.academy.scrapper;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestPropertySource(properties = {"features.orm.enabled=true"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OrmScrapperApplicationTests extends ScrapperApplicationTests {
+}
